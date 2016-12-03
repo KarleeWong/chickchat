ba4cbfb6e055f24db035339ab626058f2a87d0e7
gaurimishra <gauri.raven@gmail.com>
2016-12-03 13:13:48 -0800

    ...

--- WebApp/src/components/Chat.js
+++ WebApp/src/components/Chat.js
@@ -1,6 +1,7 @@
 import moment from "moment"
 import React, {PropTypes} from "react"
 import {connect} from "react-redux"
+import ReplyBox from "./ReplyBox"
 
 export class Chat extends React.Component {
     render () {
@@ -11,6 +12,7 @@ export class Chat extends React.Component {
                 </ul>
 
                 {/* Exercise 2: Add a ReplyBox component */}
+                <ReplyBox/>
 
             </div>
         )
@@ -22,6 +24,9 @@ function renderMessage (message) {
         <li key={message.messageId}>
 
             {/* Exercise 3: Add message author */}
+          <img src = {message.author.picture} height ="32px"/>
+            {message.author.name + ": "}
+
 
             {getMessageBody(message)}
         </li>
